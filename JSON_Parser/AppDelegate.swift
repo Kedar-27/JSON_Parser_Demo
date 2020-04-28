0a393f503397d91bf417aefa2c0009d6293009cb
Kedar Sukerkar <kedarjsukerkar@yahoo.co.in>
2020-04-28 13:23:56 +0530

    Project Setup completed

---
+++ JSON_Parser/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  JSON_Parser
+//
+//  Created by Kedar Sukerkar on 28/04/20.
+//  Copyright © 2020 Kedar-27. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    // MARK: AppDelegate Lifecycle
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: - Setup Initial VC
+    func setupInitialVC(){
+        
+    }
+    
+
+    
+
+
+}
+
